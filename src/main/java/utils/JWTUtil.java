5bf26a342468493233f3a7c8d65802e7cc332c2b
molyx <537084436>
2018-08-23 10:48:06 +0800

    修改JWT工具类

--- src/main/java/utils/JWTUtil.java
+++ src/main/java/utils/JWTUtil.java
@@ -8,63 +8,38 @@ import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.io.UnsupportedEncodingException;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class JWTUtil {
 
-    /**
-     * token秘钥，请勿泄露，请勿随便修改 backups:JKKLJOoasdlfj
-     */
     public static final String SECRET = "JKKLJOoasdlfj";
-    /**
-     * token 过期时间: 10天
-     */
     public static final long calendarField = System.currentTimeMillis();
-    public static final long calendarInterval = 2000;
+    public static final long calendarInterval = 1000*60*30;
 
     public static String createToken(Long userId) throws UnsupportedEncodingException {
-        /**
-         *  第二部分payload
-         *  iss: jwt签发者
-            sub: jwt所面向的用户
-            aud: 接收jwt的一方
-            exp: jwt的过期时间，这个过期时间必须要大于签发时间
-            nbf: 定义在什么时间之前，该jwt都是不可用的.
-            iat: jwt的签发时间
-            jti: jwt的唯一身份标识，主要用来作为一次性token,从而回避重放攻击。
-         */
-        // 第一部分的header Map
         Map<String, Object> map = new HashMap<>();
-        map.put("alg", "HS256");//声明价目算法为HMAC SHA256算法
-        map.put("typ", "JWT");//声明类型为JWT
+        map.put("alg", "HS256");
+        map.put("typ", "JWT");
 
-        String token = JWT.create().withHeader(map) // header
-                .withClaim("iss", "Service") // payload
+        String token = JWT.create().withHeader(map)
+                .withClaim("iss", "Service")
                 .withClaim("aud", "APP")
-                .withClaim("user_id", null == userId ? null : userId.toString())
-                .withIssuedAt(new Date()) // sign time
-                .withExpiresAt(new Date((calendarField + calendarInterval))) // expire time
-                .sign(Algorithm.HMAC256(SECRET)); // signature
+                .withClaim("userId", null == userId ? null : userId.toString())
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date((calendarField + calendarInterval)))
+                .sign(Algorithm.HMAC256(SECRET));
         return token;
     }
 
-    /**
-     * 解密Token
-     *
-     * @param token
-     * @return
-     * @throws Exception
-     */
     public static Map<String, Claim> verifyToken(String token) {
         DecodedJWT jwt = null;
         try {
             JWTVerifier verifier = JWT.require(Algorithm.HMAC256(SECRET)).build();
             jwt = verifier.verify(token);
         } catch (Exception e) {
-            //e.printStackTrace();
+            return null;
         }
         if (null != jwt) {
             return jwt.getClaims();
@@ -80,7 +55,7 @@ public class JWTUtil {
      */
     public static Long getAppUID(String token) {
         Map<String, Claim> claims = verifyToken(token);
-        Claim user_id_claim = claims.get("user_id");
+        Claim user_id_claim = claims.get("userId");
         if (null == user_id_claim || StringUtils.isEmpty(user_id_claim.asString())) {
             // token 校验失败, 抛出Token验证非法异常
         }
